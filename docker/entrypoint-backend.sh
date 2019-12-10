fe1951b915c47a05e344f49fe12af919973e911c
Sickenberg <romain.sickenberg@liip.ch>
2019-12-10 10:01:11 +0100

    Docker optimisations part.1

---
+++ docker/entrypoint-backend.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+pip install -r requirements/base.txt
+
+#while ! (echo > /dev/tcp/db/5432) ; do echo -n '.'; sleep 1; done;
+echo "Running migrations ..."
+./manage.py migrate
+
+echo "Creating admin account ..."
+./manage.py loaddata initial_users
+
+echo "Fixturize the env ..."
+./manage.py fixturize -y -s --no-reset
+
+
+exec "${@}"
